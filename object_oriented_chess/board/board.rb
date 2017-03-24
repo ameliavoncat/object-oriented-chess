571d5b10da89b67403bfc657f7acf2a00606bd82
Amelia Clarke <ameliavoncat@gmail.com>
2017-03-24 11:48:48 -0700

    Implements move validation for pawns

--- object_oriented_chess/board/board.rb
+++ object_oriented_chess/board/board.rb
@@ -1,14 +1,17 @@
 require_relative '../constants/board_setup'
 require_relative './square/square_content'
+require 'colorize'
 
 class Board
   def initialize(state = BOARD_SETUP)
-    puts('initializing')
+    puts('Welcome to Chess!'.white)
     @state = set_board(state)
     display
   end
 
   def set_board(state)
+    puts 'setting the board ...'.white
+    row_id = 1
     state.map {|row|
       row.map {|piece|
         SquareContent.is(piece)
@@ -20,10 +23,56 @@ class Board
     @state.each {|row|
       accumulator = []
       row.each {|square|
-        accumulator.push('[ ' + square.icon + ' ]')
+        accumulator.push('[ '.yellow + square.icon.white + ' ]'.yellow)
       }
       puts(accumulator.join)
     }
+    handle_move
+  end
+
+  def handle_move
+    move = gets.chomp.split(':')
+    start_square = translate_square(move[0])
+    target_square = translate_square(move[1])
+
+    validated = validate_move(start_square, target_square)
+    return if !validated
+    @state[target_square[:row]][target_square[:column]] =
+      @state[start_square[:row]][start_square[:column]]
+    @state[start_square[:row]][start_square[:column]] = SquareContent.is('Empty')
+    display
+  end
+
+  def get_move
+
+  end
+
+  def validate_move(start_square, target_square)
+    @state[start_square[:row]][start_square[:column]].validate(start_square, target_square)
+  end
+
+  def translate_square(square)
+    square_indices = square.split('')
+    result = {
+      :row => row_to_index(square_indices[1]),
+      :column => column_to_index(square_indices[0])
+    }
+    mark_occupied(result)
+    result
+  end
+
+  def mark_occupied(square)
+    square[:is_occupied] =
+      true if !@state[square[:row]][square[:column]].instance_of? Empty
+  end
+
+  def column_to_index(letter)
+    letters = ('A'..'H').to_a
+    letters.find_index(letter.upcase)
+  end
+
+  def row_to_index(number)
+    number.to_i - 1
   end
 
 end
--- object_oriented_chess/board/square/pieces/pawn/pawn.rb
+++ object_oriented_chess/board/square/pieces/pawn/pawn.rb
@@ -5,7 +5,20 @@ class Pawn < Piece
     super(type, color, icon)
   end
 
-  def validate_move
-    'Worked!'
+  def validate(start_square, target_square)
+    return validate_capture(start_square, target_square) if target_square[:is_occupied]
+    validate_move(start_square, target_square)
+  end
+
+  def validate_move(start_square, target_square)
+    validated = (target_square[:row] ===
+      start_square[:row] + 1 || target_square[:row] === start_square[:row] + 2) &&
+      (target_square[:column] === start_square[:column])
+  end
+
+  def validate_capture(start_square, target_square)
+    validated = (target_square[:column] ===
+      (start_square[:column] + 1 || start_square[:column] - 1)) &&
+      (target_square[:row] === start_square[:row])
   end
 end
